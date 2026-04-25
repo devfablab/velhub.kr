9a56f8a8ce9c33668d074862f0fa9395d58a0ca5
Chloe <gtalk.with.nani@gmail.com>
2026-04-25 19:44:20 +0900

    Update headerSiteGuard.tsx

--- src/app/(site)/[siteName]/headerSiteGuard.tsx
+++ src/app/(site)/[siteName]/headerSiteGuard.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 import { useMediaQuery, useTheme } from '@mui/material';
 import { normalizeText } from '@/lib/utils';
 import HeaderSite from '@/components/headers/Site';
+import { useEffect } from 'react';
 
 const HIDE_PREFIXES = ['/contents', '/design', '/manage', '/', '/team', '/stats', '/members', '/filtered'];
 
@@ -19,10 +20,17 @@ function shouldHideHeader(pathname: string, siteName: string) {
 
 export default function HeaderSiteGuard() {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
   const siteName = normalizeText(pathname.split('/')[1]);
   const theme = useTheme();
   const isNotMobile = useMediaQuery(theme.breakpoints.up('sm'));
   const isMobile = !isNotMobile;
+
+  useEffect(() => {
+    const search = searchParams.toString();
+    const currentPath = search ? `${pathname}?${search}` : pathname;
+    sessionStorage.setItem('route:returnPath', currentPath);
+  }, [pathname, searchParams]);
 
   if (!siteName || !isMobile) {
     return <HeaderSite />;
