6a9ce699dc79bd1585534ce6797d249f3e03cc3c
Chloe <gtalk.with.nani@gmail.com>
2026-05-18 04:37:59 +0900

    메인 내용 삭제

--- src/app/(lounge)/page.tsx
+++ src/app/(lounge)/page.tsx
@@ -1,14 +1,10 @@
 import styles from '../page.module.sass';
 import AuthActions from '@/components/auth/AuthActions';
-import SectionJoinSites from './home/sectionJoinSites';
-import SectionPendingInvites from './home/sectionPendingInvites';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <AuthActions />
-      <SectionPendingInvites />
-      <SectionJoinSites />
     </main>
   );
 }
