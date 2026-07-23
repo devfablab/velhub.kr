74c2b782e747e6218d7e5044dac95f04c13b8f2c
Chloe <gtalk.with.nani@gmail.com>
2026-07-24 08:47:16 +0900

    푸터 수정

--- src/components/footers/Footer.tsx
+++ src/components/footers/Footer.tsx
@@ -43,6 +43,53 @@ export default function Footer() {
               </li>
             </ul>
           </div>
+          <div className={styles.legals}>
+            <dl>
+              <div>
+                <div>
+                  <dt className={styles.hidden}>회사명</dt>
+                  <dd>데브런닷스튜디오</dd>
+                </div>
+                <div>
+                  <dt>대표자명</dt>
+                  <dd>고종길</dd>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <dt>사업자등록번호</dt>
+                  <dd>319-21-01382</dd>
+                </div>
+                <div>
+                  <dt>통신판매업 신고번호</dt>
+                  <dd>2026-서울관악-</dd>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <dt>주소</dt>
+                  <dd>
+                    <address>서울시 관악구 조원로 20길 10</address>
+                  </dd>
+                </div>
+                <div>
+                  <dt>연락처</dt>
+                  <dd>010 7154 5796</dd>
+                </div>
+              </div>
+            </dl>
+            <div>
+              <p>
+                데브런닷스튜디오는 통신판매중개자이며 통신판매의 당사자가 아닙니다. 따라서 데브런닷스튜디오는 콘텐츠,
+                거래 정보 및 거래에 대하여 책임지지 않습니다.
+              </p>
+              <p>
+                데브런닷스튜디오가 소유/운영/관리하는 웹사이트 내의 콘텐츠/회원/이벤트 정보, 디자인 및 화면의 구성, UI를
+                포함하여 일체의 콘텐츠에 대한 무단 복제, 전송, 방송, 배포, 스크래핑 등의 행위는 저작권법 및 콘텐츠산업
+                진흥법 등 관련 법령에 의하여 엄격히 금지됩니다.
+              </p>
+            </div>
+          </div>
           <div className={styles.loves}>
             <p className={styles.love} style={{ color: 'hotpink' }}>
               <FavoriteRoundedIcon /> <span>velhub</span>
